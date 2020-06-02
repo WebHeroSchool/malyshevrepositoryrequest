c783080d9b7f68d4e03cb9d7e595faac950e29ce
Maxim Malyshev <malyshev21@gmail.com>
2020-06-02 18:34:38 +0300

    fetch

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-alert ('Hello World!')
+let url = window.location.toString();
+
+let getUserName = (url) => {
+    let urlArray = url.split('=');
+    let userName = urlArray[1];
+    if (userName == undefined){
+        userName = 'malyshevmaxim';
+    }
+    return userName
+}
+
+let name = getUserName(url)
+
+fetch('https://api.github.com/users/' + name)
+  .then(res => res.json())
+  .then(json => {
+  	let avatar = json.avatar_url;
+  	let name = json.login;
+  	let bio = json.bio;
+  	let profile =json.html_url;
+
+  	let createAvatar = () => {
+  		let newAvatar = document.createElement('img');
+        newAvatar.src = avatar;
+        let addString = document.createElement('br');
+        document.body.appendChild(newAvatar);
+        document.body.appendChild(addString);
+        }
+
+    let createBio = () => {
+    	let newBio = document.createElement('p');
+    	newBio.innerHTML = bio;
+    	document.body.appendChild(newBio);
+        }
+
+    let createProfile = () => {
+    	let elementForLink = document.createElement('a');
+    	let elementForHeader = document.createElement('h2');
+    	elementForHeader.innerText = name;
+    	elementForLink.href = profile;
+    	document.body.appendChild(elementForLink);
+    	elementForLink.appendChild(elementForHeader);
+        }
+
+    createProfile();
+    createBio();
+    createAvatar();
+    })
+    .catch(err => alert(err + 'Информация о пользователе не доступна'));
