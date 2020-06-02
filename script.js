996c69118fda1eaf10c9b96cb9cab607311fa1e0
Maxim Malyshev <malyshev21@gmail.com>
2020-06-02 21:19:50 +0300

    new fetch

--- script.js
+++ script.js
@@ -18,32 +18,36 @@ fetch('https://api.github.com/users/' + name)
   	let name = json.login;
   	let bio = json.bio;
   	let profile =json.html_url;
+    if (name) {
 
-  	let createAvatar = () => {
-  		let newAvatar = document.createElement('img');
-        newAvatar.src = avatar;
-        let addString = document.createElement('br');
-        document.body.appendChild(newAvatar);
-        document.body.appendChild(addString);
+    	let createAvatar = () => {
+    		let newAvatar = document.createElement('img');
+    		newAvatar.src = avatar;
+    		let addString = document.createElement('br');
+    		document.body.appendChild(newAvatar);
+    		document.body.appendChild(addString);
+    	}
+
+    	let createBio = () => {
+    		let newBio = document.createElement('p');
+    		newBio.innerHTML = bio;
+    		document.body.appendChild(newBio);
+    	}
+
+    	let createProfile = () => {
+    		let elementForLink = document.createElement('a');
+    		let elementForHeader = document.createElement('h2');
+    		elementForHeader.innerText = name;
+    		elementForLink.href = profile;
+    		document.body.appendChild(elementForLink);
+    		elementForLink.appendChild(elementForHeader);
+    	}
+
+    	createProfile();
+    	createBio();
+    	createAvatar();
+        } else {
+        	alert('Информация о пользователе не доступна')
         }
-
-    let createBio = () => {
-    	let newBio = document.createElement('p');
-    	newBio.innerHTML = bio;
-    	document.body.appendChild(newBio);
-        }
-
-    let createProfile = () => {
-    	let elementForLink = document.createElement('a');
-    	let elementForHeader = document.createElement('h2');
-    	elementForHeader.innerText = name;
-    	elementForLink.href = profile;
-    	document.body.appendChild(elementForLink);
-    	elementForLink.appendChild(elementForHeader);
-        }
-
-    createProfile();
-    createBio();
-    createAvatar();
     })
     .catch(err => alert(err + 'Информация о пользователе не доступна'));
