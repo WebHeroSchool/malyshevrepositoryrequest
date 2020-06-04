756f879ec134fae0b007e9596b2a78f6e4e40ecd
Maxim Malyshev <malyshev21@gmail.com>
2020-06-04 11:17:51 +0300

    change

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let url = window.location.toString();
+let preloader = document.getElementById('preloader');
 
 let getUserName = (url) => {
     let urlArray = url.split('=');
@@ -11,46 +12,71 @@ let getUserName = (url) => {
 
 let name = getUserName(url)
 
-fetch('https://api.github.com/users/' + name)
-  .then(res => res.json())
-  .then(json => {
-  	let avatar = json.avatar_url;
-  	let name = json.login;
-  	let bio = json.bio;
-  	let profile = json.html_url;
-    if (name) {
 
-    	let createAvatar = () => {
-    		let newAvatar = document.createElement('img');
-    		newAvatar.src = avatar;
-    		let addString = document.createElement('br');
-    		document.body.appendChild(newAvatar);
-    		document.body.appendChild(addString);
-    	}
+let getNowDate = new Promise((resolve, reject) => {
+    let nowDate = new Date();
+    setTimeout(() => nowDate ? resolve(nowDate) : reject ('Ошибка вычисления'), 3000)
+  });
 
-    	let createBio = () => {
-    		let newBio = document.createElement('p');
-    		newBio.innerHTML = bio;
-    		document.body.appendChild(newBio);
-    	}
 
-    	let createProfile = () => {
-    		let elementForLink = document.createElement('a');
-    		let elementForHeader = document.createElement('h2');
-    		elementForHeader.innerText = name;
-    		elementForLink.href = profile;
-    		document.body.appendChild(elementForLink);
-    		elementForLink.appendChild(elementForHeader);
-    	}
+let getUserData = fetch('https://api.github.com/users/' + name);
 
-    	createProfile();
-    	createBio();
-    	createAvatar();
-        } else {
-        	alert('Информация о пользователе не доступна.');
+
+Promise.all([getUserData, getNowDate])
+    .then(([ourUserData, ourNowDate]) => {
+        userData = ourUserData;
+        currentDate = ourNowDate;
+    })
+
+  .then(res => userData.json())
+  .then(userInfo => {
+      let avatar = userInfo.avatar_url;
+      let name = userInfo.login;
+      let bio = userInfo.bio;
+      let profile =userInfo.html_url;
+      if (name) {
+
+       let createAvatar = () => {
+          let newAvatar = document.createElement('img');
+          newAvatar.src = avatar;
+          let addString = document.createElement('br');
+          document.body.appendChild(newAvatar);
+          document.body.appendChild(addString);
+          }
+
+           let createBio = () => {
+              let newBio = document.createElement('p');
+              newBio.innerHTML = bio;
+              document.body.appendChild(newBio);
+              }
+
+            let createProfile = () => {
+                let elementForLink = document.createElement('a');
+                let elementForHeader = document.createElement('h2');
+                elementForHeader.innerText = name;
+                elementForLink.href = profile;
+                document.body.appendChild(elementForLink);
+                elementForLink.appendChild(elementForHeader);
+                }
+
+                let createDate = () => {
+                let newCurrentDate = document.createElement('p');
+                newCurrentDate.innerHTML = currentDate;
+                document.body.appendChild(newCurrentDate);
+            }
+
+            preloader.style.display = 'none';
+            createProfile();
+            createBio();
+            createAvatar();
+            createDate();
+        }
+        else {
+            alert('Информация о пользователе не доступна.');
         	setTimeout(function() {
         		window.location.href = 'https://webheroschool.github.io/malyshevrepositoryrequest/';
         	}, 2000);
         }
     })
-    .catch(err => alert(err + 'Информация о пользователе не доступна!'));
+
+    .catch(err => alert(err + 'Информация о пользователе не доступна'));
